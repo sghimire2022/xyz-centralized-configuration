dd016631d64153ec4ea2e9051ec1868e87771a6b
Suresh Ghimire <sureshghimire2018@gmail.com>
2026-05-31 16:00:22 -0500

    added parameters on app yaml file

---
+++ src/main/java/dev/sghimire/xyz_centralized_configuration/XyzCentralizedConfigurationApplication.java
@@ -0,0 +1,13 @@
+package dev.sghimire.xyz_centralized_configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzCentralizedConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzCentralizedConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sghimire/xyz_centralized_configuration/XyzCentralizedConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sghimire.xyz_centralized_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzCentralizedConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
